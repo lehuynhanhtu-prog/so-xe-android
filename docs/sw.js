b1f601b5090d7e9f5543dfaa0d9377b10e10f23b
lehuynhanhtu-prog <lehuynhanhtu@gmail.com>
2026-09-11 11:01:16 +0700

    Move app download link into Settings

--- docs/sw.js
+++ docs/sw.js
@@ -1,5 +1,5 @@
-const CACHE='so-xe-github-v17';
-const ASSETS=['./','./index.html','./downloads.html','./styles.css?v=17','./app.js?v=17','./privacy.html','./terms.html','./manifest.webmanifest','./icons/icon.svg','./downloads/So-Xe-iOS.mobileconfig'];
+const CACHE='so-xe-github-v18';
+const ASSETS=['./','./index.html','./downloads.html','./styles.css?v=17','./app.js?v=18','./privacy.html','./terms.html','./manifest.webmanifest','./icons/icon.svg','./downloads/So-Xe-iOS.mobileconfig'];
 self.addEventListener('install',event=>{self.skipWaiting();event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS))) });
 self.addEventListener('activate',event=>event.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))),self.clients.claim()])));
 self.addEventListener('fetch',event=>{if(event.request.method!=='GET'||new URL(event.request.url).origin!==location.origin)return;event.respondWith(fetch(event.request,{cache:'no-store'}).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match(event.request).then(cached=>cached||caches.match('./index.html'))))});
