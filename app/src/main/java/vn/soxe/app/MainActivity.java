d58d87586db9e905b21e658a4e3817a8641f1cdf
lehuynhanhtu-prog <lehuynhanhtu@gmail.com>
2026-09-15 08:45:55 +0700

    Add hybrid vehicles and adaptive monthly reports

--- app/src/main/java/vn/soxe/app/MainActivity.java
+++ app/src/main/java/vn/soxe/app/MainActivity.java
@@ -256,7 +256,7 @@ public class MainActivity extends ComponentActivity {
 
         @JavascriptInterface
         public String appVersion() {
-            return "2.0.6";
+            return "2.0.7";
         }
     }
 
