7ed78824367730be2cce5a07f10d10f8ed2c24cc
lehuynhanhtu-prog <lehuynhanhtu@gmail.com>
2026-09-18 10:21:20 +0700

    Open dashboard reminders and summarize vehicle service and documents

--- app/src/main/java/vn/soxe/app/MainActivity.java
+++ app/src/main/java/vn/soxe/app/MainActivity.java
@@ -380,7 +380,7 @@ public class MainActivity extends ComponentActivity {
 
         @JavascriptInterface
         public String appVersion() {
-            return "2.0.14";
+            return "2.0.15";
         }
     }
 
